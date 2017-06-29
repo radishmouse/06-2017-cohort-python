edab20d358cda8582274c52f6e683fa170ae1e28
Chris Aquino <caquino@bignerdranch.com>
2017-06-29 07:09:39 -0400

    adds friends, fixes print_contact_info

--- 201 - objects/person.py
+++ 201 - objects/person.py
@@ -3,12 +3,14 @@ class Person(object):
     self.name = name
     self.email = email
     self.phone = phone
+    self.friends = []
   
   def greet(self, other_person):
     print 'Hello %s, I am %s' % (other_person.name, self.name)
 
-  def print_contact_info():
+  def print_contact_info(self):
     print "%s's email: %s, %s's phone number: %s" % (self.name, self.email, self.name, self.phone)
+  
 
 
 sonny = Person('Sonny', 'sonny@hotmail.com', '483-485-4948')
@@ -19,3 +21,10 @@ jordan.greet(sonny)
 
 print "%s - %s" % (sonny.email, sonny.phone)
 print "%s - %s" % (jordan.email, jordan.phone)
+
+sonny.print_contact_info()
+jordan.print_contact_info()
+
+jordan.friends.append(sonny)
+sonny.friends.append(jordan)
+
